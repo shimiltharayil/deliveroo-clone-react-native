0f8eef7d65a537bcf74ad18155a4e8c853dc4649
Shimil <shimiltharayil007@gmail.com>
2022-12-17 10:08:56 +0530

    completed

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from './screens/HomeScreen';
